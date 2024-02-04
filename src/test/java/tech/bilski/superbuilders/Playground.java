bb33f5df23ed550c61e56e99ffaf7de97cc00d25
Jacek Bilski <jacek@bilski.tech>
2024-02-04 22:30:01 +0100

    I can create AnnotationProcessor, and it's called during tests execution. Still, not doing anything.

---
+++ src/test/java/tech/bilski/superbuilders/Playground.java
@@ -0,0 +1,12 @@
+package tech.bilski.superbuilders;
+
+import org.junit.jupiter.api.Test;
+
+@SuperBuilder
+public class Playground {
+
+  @Test
+  void sth() {
+    System.out.println("foo");
+  }
+}
