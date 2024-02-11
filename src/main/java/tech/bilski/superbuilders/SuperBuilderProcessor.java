99dc4a209678e93075b3711e7466b3d03a5641ca
Jacek Bilski <jacek@bilski.tech>
2024-02-11 11:37:14 +0100

    I can also create an inner class.

--- src/main/java/tech/bilski/superbuilders/SuperBuilderProcessor.java
+++ src/main/java/tech/bilski/superbuilders/SuperBuilderProcessor.java
@@ -5,9 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spoon.processing.AbstractAnnotationProcessor;
 import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.ModifierKind;
-import spoon.reflect.reference.CtTypeReference;
 
 public class SuperBuilderProcessor extends AbstractAnnotationProcessor<SuperBuilder, CtClass<?>> {
 
@@ -16,14 +14,8 @@ public class SuperBuilderProcessor extends AbstractAnnotationProcessor<SuperBuil
   @Override
   public void process(SuperBuilder annotation, CtClass<?> element) {
     log.warn("Processing element{}", element);
-//    var className = element.getSimpleName();
-//    CtClass builder = getFactory().Core().createClass();
-//    builder.setSimpleName("JavaBeanBuilder");
-//    builder.addModifier(ModifierKind.STATIC);
-//    element.addNestedType(builder);
-
-    CtTypeReference<Integer> intType = getFactory().Code().createCtTypeReference(Integer.class);
-    CtField intField = getFactory().createField(element, Set.of(ModifierKind.PRIVATE), intType, "x");
-    element.addField(intField);
+    CtClass builder = getFactory().createClass(element, "JavaBeanBuilder");
+    builder.setModifiers(Set.of(ModifierKind.STATIC, ModifierKind.PUBLIC));
+    element.addNestedType(builder);
   }
 }
--- src/test/java/tech/bilski/superbuilders/JavaBeanTests.java
+++ src/test/java/tech/bilski/superbuilders/JavaBeanTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import spoon.Launcher;
 import spoon.SpoonAPI;
-import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtType;
+import spoon.testing.Assert;
 
-import static spoon.testing.Assert.assertThat;
 
 public class JavaBeanTests {
 
@@ -15,8 +14,8 @@ public class JavaBeanTests {
   @Disabled
   void oneField_withConstructorParameter() {
 //    String str = UUID.randomUUID().toString();
-//    JavaBean javaBean = new JavaBeanBuilder().withStr(str).build();
-//    assertThat(javaBean).hasFieldOrPropertyWithValue("str", str);
+//    JavaBean javaBean = new JavaBean.JavaBeanBuilder().build();
+//    Assertions.assertThat(javaBean).hasFieldOrPropertyWithValue("str", str);
   }
 
   @Test
@@ -25,13 +24,12 @@ public class JavaBeanTests {
     spoon.run();
 
     CtType<SuperBuilderProcessor> type = spoon.getFactory().Type().get(JavaBean.class);
-//    CtType<?> javaBeanBuilder = type.getNestedType("JavaBeanBuilder");
-//    assertThat(javaBeanBuilder)
-//        .withProcessor(new SuperBuilderProcessor())
-//        .isEqualTo("private static class JavaBeanBuilder");
-    CtField<?> x = type.getField("x");
-    assertThat(x)
-        .withProcessor(new SuperBuilderProcessor())
-        .isEqualTo("private java.lang.Integer x;");
+    CtType<?> javaBeanBuilder = type.getNestedType("JavaBeanBuilder");
+    System.out.println(javaBeanBuilder);
+    Assert.assertThat(javaBeanBuilder)
+        .isEqualTo("public static class JavaBeanBuilder {\n"
+            + "    public JavaBeanBuilder() {\n"
+            + "    }\n"
+            + "}");
   }
 }
