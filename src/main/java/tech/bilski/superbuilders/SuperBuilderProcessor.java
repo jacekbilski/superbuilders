b179a59dc4d7850b42c5772dc2ea7b273477a935
Jacek Bilski <jacek@bilski.tech>
2024-02-11 10:44:23 +0100

    I can add a field to a class.

--- src/main/java/tech/bilski/superbuilders/SuperBuilderProcessor.java
+++ src/main/java/tech/bilski/superbuilders/SuperBuilderProcessor.java
@@ -1,9 +1,13 @@
 package tech.bilski.superbuilders;
 
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spoon.processing.AbstractAnnotationProcessor;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.reference.CtTypeReference;
 
 public class SuperBuilderProcessor extends AbstractAnnotationProcessor<SuperBuilder, CtClass<?>> {
 
@@ -12,5 +16,14 @@ public class SuperBuilderProcessor extends AbstractAnnotationProcessor<SuperBuil
   @Override
   public void process(SuperBuilder annotation, CtClass<?> element) {
     log.warn("Processing element{}", element);
+//    var className = element.getSimpleName();
+//    CtClass builder = getFactory().Core().createClass();
+//    builder.setSimpleName("JavaBeanBuilder");
+//    builder.addModifier(ModifierKind.STATIC);
+//    element.addNestedType(builder);
+
+    CtTypeReference<Integer> intType = getFactory().Code().createCtTypeReference(Integer.class);
+    CtField intField = getFactory().createField(element, Set.of(ModifierKind.PRIVATE), intType, "x");
+    element.addField(intField);
   }
 }
--- src/test/java/tech/bilski/superbuilders/JavaBeanTests.java
+++ src/test/java/tech/bilski/superbuilders/JavaBeanTests.java
@@ -2,6 +2,12 @@ package tech.bilski.superbuilders;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import spoon.Launcher;
+import spoon.SpoonAPI;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtType;
+
+import static spoon.testing.Assert.assertThat;
 
 public class JavaBeanTests {
 
@@ -11,5 +17,21 @@ public class JavaBeanTests {
 //    String str = UUID.randomUUID().toString();
 //    JavaBean javaBean = new JavaBeanBuilder().withStr(str).build();
 //    assertThat(javaBean).hasFieldOrPropertyWithValue("str", str);
+  }
+
+  @Test
+  void classHasInnerBuilderClass() {
+    SpoonAPI spoon = new Launcher();
+    spoon.run();
+
+    CtType<SuperBuilderProcessor> type = spoon.getFactory().Type().get(JavaBean.class);
+//    CtType<?> javaBeanBuilder = type.getNestedType("JavaBeanBuilder");
+//    assertThat(javaBeanBuilder)
+//        .withProcessor(new SuperBuilderProcessor())
+//        .isEqualTo("private static class JavaBeanBuilder");
+    CtField<?> x = type.getField("x");
+    assertThat(x)
+        .withProcessor(new SuperBuilderProcessor())
+        .isEqualTo("private java.lang.Integer x;");
   }
 }
